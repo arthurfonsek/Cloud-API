a559ca787bc4acda52cb800fd0c1059f1ceb3aaa
Arthur Fonseca <81189803+arthurfonsek@users.noreply.github.com>
2023-11-23 16:37:10 -0300

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, schemas, crud
+from .database import SessionLocal, engine
+#
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/quotes/", response_model=schemas.Member)
+def create_member(member: schemas.MemberCreate, db: Session = Depends(get_db)):
+    return crud.create_member(db=db, member=member)
+
+@app.get("/")
+def hello_world():
+    return "Hello World"
+
+@app.get("/quotes/", response_model=list[schemas.Member])
+def read_members(db: Session = Depends(get_db)):
+    members = crud.get_members(db)
+    return members
+
+@app.get("/quotes/{member_id}", response_model=schemas.Member)
+def read_member(member_id: int, db: Session = Depends(get_db)):
+    db_member = crud.get_member(db, member_id=member_id)
+    if db_member is None:
+        raise HTTPException(status_code=404, detail="Member not found")
+    return db_member
+
+@app.put("/quotes/{member_id}", response_model=schemas.Member)
+def update_member(member_id: int, member: schemas.MemberUpdate, db: Session = Depends(get_db)):
+    db_member = crud.get_member(db, member_id=member_id)
+    if db_member is None:
+        raise HTTPException(status_code=404, detail="Member not found")
+    return crud.update_member(db=db, member=member, member_id=member_id)
+
+@app.delete("/quotes/{member_id}", response_model=schemas.DeletedResponse)
+def delete_member(member_id: int, db: Session = Depends(get_db)):
+    deleted = crud.delete_member(db=db, member_id=member_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Member not found")
+    return {"message": "Member deleted successfully", "deleted_member_id": member_id}
